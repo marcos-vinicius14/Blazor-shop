8aacb91d8d061c7de8c8490dcde89319092e3e2c
MarcosVinicius <marcusvinycius73@gmail.com>
2024-01-31 20:16:53 -0300

    auth e auten

---
+++ BlazorShop/Models/CategoryModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
+
+public class CategoryModel
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "Informe p título.")]
+    [MaxLength(120, ErrorMessage = "A título deve conter no máximo 60 caracteres.")]
+    [MinLength(5, ErrorMessage = "O título deve conter no mínimo 5 caracteres.")]
+    public string Title { get; set; } = String.Empty;
+}
---
+++ BlazorShop/Models/ProductModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorShop.Models;
+
+public class ProductModel
+{
+    [Key]
+    public int Id { get; set; }
+    
+    [Required(ErrorMessage = "O produto deve conter um nome.")]
+    [MaxLength(120, ErrorMessage = "O titulo deve conter no máximo 120 caracteres")]
+    [MinLength(5, ErrorMessage = "O minímo de caracteres é 5.")]
+    public string Title { get; set; } = String.Empty;
+    public string? Description { get; set; }
+    
+    [Required(ErrorMessage = "Informe um preço.")]
+    [DataType(DataType.Currency)]
+    public decimal Price { get; set; }
+
+    public int CategoryId { get; set; }
+    public CategoryModel Category { get; set; } = null!;
+}
